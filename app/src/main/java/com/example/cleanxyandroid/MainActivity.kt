5c6ff4e88e049181c63c5852fd5a8d3839dbf99f
Shantanu Gupta <Gupta.Shantanu6@gmail.com>
2021-07-07 16:05:16 +0530

    bottom nav bar initial setup

--- app/src/main/java/com/example/cleanxyandroid/MainActivity.kt
+++ app/src/main/java/com/example/cleanxyandroid/MainActivity.kt
@@ -3,6 +3,13 @@ package com.example.cleanxyandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.cleanxyandroid.bottomNavFragments.HistoryFragment
+import com.example.cleanxyandroid.bottomNavFragments.HomeFragment
+import com.example.cleanxyandroid.bottomNavFragments.NotificationsFragment
+import com.example.cleanxyandroid.bottomNavFragments.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +22,35 @@ class MainActivity : AppCompatActivity() {
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
         window.statusBarColor = this.resources.getColor(R.color.appBlue)
 
+        val homeFragment = HomeFragment()
+        val notificationsFragment = NotificationsFragment()
+        val historyFragment = HistoryFragment()
+        val profileFragment = ProfileFragment()
+        
+        setCurrentFragment(homeFragment)
+
+        val bottomNav : BottomNavigationView = findViewById(R.id.bottomNavBar)
+
+        bottomNav.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.homeIcon -> setCurrentFragment(homeFragment)
+                R.id.notificationsIcon -> setCurrentFragment(notificationsFragment)
+                R.id.historyIcon -> setCurrentFragment(historyFragment)
+                R.id.profileIcon -> setCurrentFragment(profileFragment)
+            }
+            true
+        }
+
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frameHome, fragment)
+            commit()
+        }
+    }
+
+    override fun onBackPressed() {
+        Toast.makeText(this@MainActivity, "Please log out to go back", Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/cleanxyandroid/TempLogin.kt
+++ app/src/main/java/com/example/cleanxyandroid/TempLogin.kt
@@ -1,11 +1,13 @@
 package com.example.cleanxyandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.viewpager.widget.ViewPager
 import com.example.cleanxyandroid.adapters.CredSplashAdapter
 import com.google.android.material.tabs.TabLayout
+import com.google.firebase.auth.FirebaseAuth
 
 class TempLogin : AppCompatActivity() {
 
@@ -53,4 +55,15 @@ class TempLogin : AppCompatActivity() {
         })
 
     }
+
+    override fun onStart() {
+        super.onStart()
+        if (FirebaseAuth.getInstance().currentUser != null) {
+            val intent = Intent(this@TempLogin, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finish()
+        }
+    }
+
 }
