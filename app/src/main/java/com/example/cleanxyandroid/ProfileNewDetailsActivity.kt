c06f11c963947cfd13b0a386f1f4cbfe49210e7d
Shantanu Gupta <Gupta.Shantanu6@gmail.com>
2021-07-17 22:34:34 +0530

    image selector (profile edit activity)

--- app/src/main/java/com/example/cleanxyandroid/ProfileNewDetailsActivity.kt
+++ app/src/main/java/com/example/cleanxyandroid/ProfileNewDetailsActivity.kt
@@ -1,11 +1,22 @@
 package com.example.cleanxyandroid
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.WindowManager
+import android.widget.ImageView
+import de.hdodenhof.circleimageview.CircleImageView
 
 @Suppress("DEPRECATION")
 class ProfileNewDetailsActivity : AppCompatActivity() {
+
+    private val pickImage = 100
+    private var imageUri : Uri? = null
+
+    private lateinit var pfpView : CircleImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_new_details)
@@ -16,5 +27,22 @@ class ProfileNewDetailsActivity : AppCompatActivity() {
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
         window.statusBarColor = this.resources.getColor(R.color.appBlue)
 
+        pfpView = findViewById(R.id.pfpViewNewDetProfileActivity)
+
+        val selectImgBtn : ImageView = findViewById(R.id.imageSelectBtnNewDetProfActivity)
+        selectImgBtn.setOnClickListener {
+            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            startActivityForResult(gallery, pickImage)
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == pickImage) {
+            imageUri = data?.data
+            pfpView.setImageURI(imageUri)
+
+        }
     }
 }
