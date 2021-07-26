51116610e7d3bfd4013f5d80f5c5d13123d98808
Shantanu Gupta <Gupta.Shantanu6@gmail.com>
2021-07-26 23:56:26 +0530

    maid feedback activity design

--- app/src/main/java/com/example/cleanxyandroid/MaidFeedbackActivity.kt
+++ app/src/main/java/com/example/cleanxyandroid/MaidFeedbackActivity.kt
@@ -1,11 +1,27 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.cleanxyandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.ImageView
 
 class MaidFeedbackActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maid_feedback)
+
+        supportActionBar?.hide()
+        val window = this.window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.statusBarColor = this.resources.getColor(R.color.appBlue)
+
+        val backbtn : ImageView = findViewById(R.id.backBtnMaidFeedbackActivity)
+        backbtn.setOnClickListener {
+            onBackPressed()
+        }
+
     }
 }
--- app/src/main/java/com/example/cleanxyandroid/bottomNavFragments/HistoryFragment.kt
+++ app/src/main/java/com/example/cleanxyandroid/bottomNavFragments/HistoryFragment.kt
@@ -1,10 +1,13 @@
 package com.example.cleanxyandroid.bottomNavFragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.example.cleanxyandroid.MaidFeedbackActivity
 import com.example.cleanxyandroid.R
 
 class HistoryFragment : Fragment() {
@@ -14,8 +17,14 @@ class HistoryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_history, container, false)
 
-        return inflater.inflate(R.layout.fragment_history, container, false)
+        val tempBtn : Button = view.findViewById(R.id.tempButtonHistoryFragment)
+        tempBtn.setOnClickListener {
+            startActivity(Intent(requireContext(), MaidFeedbackActivity::class.java))
+        }
+
+        return view
 
     }
 
