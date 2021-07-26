89eca989fb29b8e03645c419de339478f366b899
Shantanu Gupta <Gupta.Shantanu6@gmail.com>
2021-07-27 01:14:18 +0530

    rating maids - Maid Feedback Activity

--- app/src/main/java/com/example/cleanxyandroid/MaidFeedbackActivity.kt
+++ app/src/main/java/com/example/cleanxyandroid/MaidFeedbackActivity.kt
@@ -4,11 +4,29 @@ package com.example.cleanxyandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 class MaidFeedbackActivity : AppCompatActivity() {
+
+    private lateinit var star1 : ImageView
+    private lateinit var star2 : ImageView
+    private lateinit var star3 : ImageView
+    private lateinit var star4 : ImageView
+    private lateinit var star5 : ImageView
+
+    private lateinit var check1 : ImageView
+    private lateinit var check2 : ImageView
+    private lateinit var check3 : ImageView
+    private lateinit var check4 : ImageView
+    private lateinit var check5 : ImageView
+    private lateinit var check6 : ImageView
+    private lateinit var check7 : ImageView
+    private lateinit var check8 : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maid_feedback)
@@ -30,5 +48,78 @@ class MaidFeedbackActivity : AppCompatActivity() {
         q2MainText.isSelected = true
         q1MainText.isSelected = true
 
+        star1 = findViewById(R.id.star1MaidFeedbackActivity)
+        star2 = findViewById(R.id.star2MaidFeedbackActivity)
+        star3 = findViewById(R.id.star3MaidFeedbackActivity)
+        star4 = findViewById(R.id.star4MaidFeedbackActivity)
+        star5 = findViewById(R.id.star5MaidFeedbackActivity)
+
+        star1.setOnClickListener {
+            changeStarToYellow(star1)
+            changeStarToWhite(star2)
+            changeStarToWhite(star3)
+            changeStarToWhite(star4)
+            changeStarToWhite(star5)
+        }
+
+        star2.setOnClickListener {
+            changeStarToYellow(star1)
+            changeStarToYellow(star2)
+            changeStarToWhite(star3)
+            changeStarToWhite(star4)
+            changeStarToWhite(star5)
+        }
+
+        star3.setOnClickListener {
+            changeStarToYellow(star1)
+            changeStarToYellow(star2)
+            changeStarToYellow(star3)
+            changeStarToWhite(star4)
+            changeStarToWhite(star5)
+        }
+
+        star4.setOnClickListener {
+            changeStarToYellow(star1)
+            changeStarToYellow(star2)
+            changeStarToYellow(star3)
+            changeStarToYellow(star4)
+            changeStarToWhite(star5)
+        }
+
+        star5.setOnClickListener {
+            changeStarToYellow(star1)
+            changeStarToYellow(star2)
+            changeStarToYellow(star3)
+            changeStarToYellow(star4)
+            changeStarToYellow(star5)
+        }
+
+        check1 = findViewById(R.id.checkSq1MaidFeedbackActivity)
+        check2 = findViewById(R.id.checkSq2MaidFeedbackActivity)
+        check3 = findViewById(R.id.checkSq3MaidFeedbackActivity)
+        check4 = findViewById(R.id.checkSq4MaidFeedbackActivity)
+        check5 = findViewById(R.id.checkSq5MaidFeedbackActivity)
+        check6 = findViewById(R.id.checkSq6MaidFeedbackActivity)
+        check7 = findViewById(R.id.checkSq7MaidFeedbackActivity)
+        check8 = findViewById(R.id.checkSq8MaidFeedbackActivity)
+
+    }
+
+    private fun changeStarToWhite(star: ImageView?) {
+        star?.setImageDrawable(
+            ContextCompat.getDrawable(
+                applicationContext,
+                R.drawable.ic_white_star_cxy
+            )
+        )
+    }
+
+    private fun changeStarToYellow(star: ImageView?) {
+        star?.setImageDrawable(
+            ContextCompat.getDrawable(
+                applicationContext,
+                R.drawable.ic_yellow_star_cxy
+            )
+        )
     }
 }
