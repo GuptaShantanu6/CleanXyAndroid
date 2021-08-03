dc4b4914351d02c2c9b37ce19f6d8ea0f8c4d2ed
Shantanu Gupta <Gupta.Shantanu6@gmail.com>
2021-08-03 18:06:22 +0530

    Time picker set - Schedule Slot

--- app/src/main/java/com/example/cleanxyandroid/ScheduleSlotActivity.kt
+++ app/src/main/java/com/example/cleanxyandroid/ScheduleSlotActivity.kt
@@ -1,15 +1,23 @@
 package com.example.cleanxyandroid
 
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.TimePicker
+import kotlin.math.min
 
 class ScheduleSlotActivity : AppCompatActivity() {
 
     private var currentMaid : Int = 1
     private var currentHour : Int = 1
+
+    private lateinit var timePicker : TimePickerDialog
+    private lateinit var hours : String
+    private lateinit var minutes : String
+    private lateinit var amOrPm : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,6 +64,53 @@ class ScheduleSlotActivity : AppCompatActivity() {
             if (currentHour < 5) {
                 hourStatText.text = (++currentHour).toString()
             }
+        }
+
+        val timeStatText : TextView = findViewById(R.id.timeDigTextScheduleSlotActivity)
+
+        val timePickerDialogListener : TimePickerDialog.OnTimeSetListener =
+            TimePickerDialog.OnTimeSetListener { _, hourOfDay, minute ->
+
+                minutes = if (minute < 10 ) {
+                    "0$minute"
+                } else  {
+                    minute.toString()
+                }
+
+                when {
+                    hourOfDay == 0 -> {
+                        hours = (12+hourOfDay).toString()
+                        amOrPm =  "am"
+                    }
+                    hourOfDay > 12 -> {
+                        hours = (hourOfDay-12).toString()
+                        amOrPm = "pm"
+                    }
+                    hourOfDay == 12 -> {
+                        hours = hourOfDay.toString()
+                        amOrPm = "pm"
+                    }
+                    else -> {
+                        hours = hourOfDay.toString()
+                        amOrPm = "am"
+                    }
+                }
+
+                timeStatText.text = getString(R.string.new_time, hours, minutes)
+
+            }
+
+        timeStatText.setOnClickListener {
+            timePicker = TimePickerDialog(
+                this@ScheduleSlotActivity,
+                timePickerDialogListener,
+                12,
+                10,
+                false
+            )
+
+            timePicker.show()
+
         }
 
     }
