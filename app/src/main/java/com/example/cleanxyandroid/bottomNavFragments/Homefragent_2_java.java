4c6a41df81ff069224b752430b2c6c8ad09e31bb
vishal R <vishal206ani@gmail.com>
2021-08-23 19:35:18 +0530

    timmer activity layout done

--- app/src/main/java/com/example/cleanxyandroid/bottomNavFragments/Homefragent_2_java.java
+++ app/src/main/java/com/example/cleanxyandroid/bottomNavFragments/Homefragent_2_java.java
@@ -1,7 +1,7 @@
 package com.example.cleanxyandroid.bottomNavFragments;
 
 import android.app.AlertDialog;
-import android.graphics.drawable.ColorDrawable;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,14 +11,12 @@ import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.cleanxyandroid.R;
+import com.example.cleanxyandroid.timerActivity;
 
 import java.util.Locale;
 
@@ -49,65 +47,69 @@ public class Homefragent_2_java extends Fragment {
         imageButton_timer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder builder=new AlertDialog.Builder(getContext());
-                builder.setCancelable(true);
+                startActivity(new Intent(getActivity(), timerActivity.class));
 
-                View addView=LayoutInflater.from(getContext()).inflate(R.layout.time_tacking_pop_up,null);
 
-                btn_start=addView.findViewById(R.id.btn_start_timer);
-                btn_stop=addView.findViewById(R.id.btn_stop_timer);
 
-                btn_close_timer=addView.findViewById(R.id.btn_close_timer);
-                time=addView.findViewById(R.id.txt_Time);
-
-                btn_close_timer.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        alertDialog.dismiss();
-                        second=0;
-                    }
-                });
-                btn_start.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-//                        runtimer();
-//                        time.setText("sdsdsd");
-                        running=true;
-                        final Handler handler=new Handler();
-                        handler.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                int hr=second/3600;
-                                int min=(second%3600)/60;
-                                int secs=second%60;
-
-                                String showTime=String.format(Locale.getDefault(),"%02d:%02d:%02d",hr,min,secs);
-                                time.setText(showTime);
-
-                                if(running){
-                                    second++;
-                                }
-                                handler.postDelayed(this,1000);
-
-                            }
-                        });
-                    }
-                });
-
-                btn_stop.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v){
-                            running=false;
-                    }
-                });
-
-//                alertDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+//                AlertDialog.Builder builder=new AlertDialog.Builder(getContext());
+//                builder.setCancelable(true);
+//
+//                View addView=LayoutInflater.from(getContext()).inflate(R.layout.time_tacking_pop_up,null);
+//
+//                btn_start=addView.findViewById(R.id.btn_start_timer);
+//                btn_stop=addView.findViewById(R.id.btn_stop_timer);
+//
+//                btn_close_timer=addView.findViewById(R.id.btn_close_timer);
+//                time=addView.findViewById(R.id.txt_Time);
+//
+//                btn_close_timer.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        alertDialog.dismiss();
+//                        second=0;
+//                    }
+//                });
+//                btn_start.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+////                        runtimer();
+////                        time.setText("sdsdsd");
+//                        running=true;
+//                        final Handler handler=new Handler();
+//                        handler.post(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                int hr=second/3600;
+//                                int min=(second%3600)/60;
+//                                int secs=second%60;
+//
+//                                String showTime=String.format(Locale.getDefault(),"%02d:%02d:%02d",hr,min,secs);
+//                                time.setText(showTime);
+//
+//                                if(running){
+//                                    second++;
+//                                }
+//                                handler.postDelayed(this,1000);
+//
+//                            }
+//                        });
+//                    }
+//                });
+//
+//                btn_stop.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v){
+//                            running=false;
+//                    }
+//                });
+//
+////                alertDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+////                alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+//
+//                builder.setView(addView);
+//                alertDialog=builder.create();
 //                alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
-
-                builder.setView(addView);
-                alertDialog=builder.create();
-                alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
-                alertDialog.show();
+//                alertDialog.show();
             }
 
             private void runtimer() {
