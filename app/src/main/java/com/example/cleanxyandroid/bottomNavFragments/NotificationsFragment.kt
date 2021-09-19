249683967bb0d8481a0a9c4bf2573065bc4610bf
vishal R <vishal206ani@gmail.com>
2021-09-19 21:28:24 +0530

    notification fragment UI DONE

--- app/src/main/java/com/example/cleanxyandroid/bottomNavFragments/NotificationsFragment.kt
+++ app/src/main/java/com/example/cleanxyandroid/bottomNavFragments/NotificationsFragment.kt
@@ -1,15 +1,26 @@
 package com.example.cleanxyandroid.bottomNavFragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.cleanxyandroid.R
+import com.example.cleanxyandroid.notificationRecylerAdapter
+import com.example.cleanxyandroid.notification_data
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.util.*
 
 class NotificationsFragment : Fragment() {
+
+    private var notificationData: ArrayList<notification_data>? = null
+    private var notificationRecycler: RecyclerView? = null
+    private var tv_notification_count: TextView? =null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,7 +35,44 @@ class NotificationsFragment : Fragment() {
             botNav.selectedItemId = R.id.homeIcon
         }
 
+        notificationData = java.util.ArrayList()
+        notificationRecycler = view.findViewById<RecyclerView>(R.id.notificationRecycler)
+
+        tv_notification_count=view.findViewById(R.id.notification_count)
+
+        setUserInfo()
+        setAdapter()
+
         return view
+    }
+
+    private fun setAdapter() {
+        val adapter = notificationRecylerAdapter(notificationData)
+        val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(context)
+
+        tv_notification_count?.setText(notificationData?.size.toString())
+
+        notificationRecycler?.setLayoutManager(layoutManager)
+        notificationRecycler?.setItemAnimator(DefaultItemAnimator())
+        notificationRecycler?.setAdapter(adapter)
+
+
+    }
+
+    private fun setUserInfo() {
+
+        notificationData?.add(
+            notification_data(
+                "Your slot with Niranjana has been confirmed ",
+                "16:27"
+            )
+        )
+        notificationData?.add(
+            notification_data(
+                "Today’s special offer :  Laundry work 25 % off.  Valid till 20th May ",
+                "16:27"
+            )
+        )
     }
 
 }
